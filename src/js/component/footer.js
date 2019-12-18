5b8f8f8a9d128fecfd2d6380c6cc2e2bb90af40d
jesus462 <jesusponce.462@gmail.com>
2019-12-18 23:08:13 +0000

    first

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<div>
+		<div className="card text-white bg-success mb-3" style={{ "max-width": 18 + "rem" }}>
+			<div className="card-header">Header</div>
+			<div className="card-body">
+				<h5 className="card-title">Success card title</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make up the bulk of the cards content.
+				</p>
+			</div>
+		</div>
+	</div>
 );
