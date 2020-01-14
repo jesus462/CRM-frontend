5a97c7456232ede6fb98e8f9ba6cff0058181add
jesus462 <jesusponce.462@gmail.com>
2020-01-14 00:43:45 +0000

    Funnel view

--- src/js/component/StepsCol.js
+++ src/js/component/StepsCol.js
@@ -6,33 +6,28 @@ import { AddClients } from "./AddClients";
 export const StepsCol = () => (
 	<div className="funnel-flex">
 		<div className="funnel-flex-column border">
-			<div className="card-header">Featured</div>
+			<div className="card-header funnel-header">Paso 1</div>
 			<ClientCards />
 			<AddClients />
 		</div>
 		<div className="funnel-flex-column border">
-			<div className="card-header">Featured</div>
+			<div className="card-header funnel-header">Paso 2</div>
 			<AddClients />
 		</div>
 		<div className="funnel-flex-column border">
-			<div className="card-header">Featured</div>
+			<div className="card-header funnel-header">Paso 3</div>
 			<AddClients />
 		</div>
 		<div className="funnel-flex-column border">
-			<div className="card-header">Featured</div>
+			<div className="card-header funnel-header">Paso 4</div>
 			<AddClients />
 		</div>
 		<div className="funnel-flex-column border">
-			<div className="card-header">Featured</div>
+			<div className="card-header funnel-header">Paso 5</div>
 			<AddClients />
 		</div>
 		<div>
-			<button
-				type="button"
-				className="btn btn-primary"
-				data-toggle="button"
-				aria-pressed="false"
-				autoComplete="off">
+			<button type="button" className="btn stitched" data-toggle="button" aria-pressed="false" autoComplete="off">
 				Add Column
 			</button>
 		</div>
