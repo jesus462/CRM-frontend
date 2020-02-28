29908d9310623b348e0d7fda00b7811b3c64a5a6
Johneg02 <jonathanegarciasilveira@gmail.com>
2020-02-28 04:31:11 +0000

    fixed again

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import Image from "react-bootstrap/Image";
 
 export const Home = () => (
 	<div>
@@ -9,36 +10,41 @@ export const Home = () => (
 				<p>1</p>
 			</div>
 			<div className="cell-home2">
-				<p>2</p>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">Compás CRM</h1>
+						<p className="lead">
+							Compás CRM lo ayuda a interactuar con clientes potenciales y clientes,obtener información
+							acerca de su negocio, crear un proceso de ventas escalable y hacer crecer su empresa más
+							rápido.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		<div className="container-card-home">
-			<div className="row">
-				<div className="col-sm-6">
-					<div className="card">
-						<div className="card-body">
-							<h5 className="card-title">Special title treatment</h5>
-							<p className="card-text">
-								With supporting text below as a natural lead-in to additional content.
-							</p>
-							<a href="#" className="btn btn-primary">
-								Go somewhere
-							</a>
-						</div>
+		<div className="row">
+			<div className="col-sm-6">
+				<div className="card">
+					<div className="card-body">
+						<h5 className="card-title">Centro de asistencia para PYMEs</h5>
+						<p className="card-text">
+							Déjanos ayudarte a encontrar la forma perfecta de llegar a tus clientes.
+						</p>
+						<a href="#" className="btn btn-success">
+							Ir
+						</a>
 					</div>
 				</div>
-				<div className="col-sm-6">
-					<div className="card">
-						<div className="card-body">
-							<h5 className="card-title">Special title treatment</h5>
-							<p className="card-text">
-								With supporting text below as a natural lead-in to additional content.
-							</p>
-							<a href="#" className="btn btn-primary">
-								Go somewhere
-							</a>
-						</div>
+			</div>
+			<div className="col-sm-6">
+				<div className="card">
+					<div className="card-body">
+						<h5 className="card-title">Centro de asistencia para Freelancers</h5>
+						<p className="card-text">Dedícate a desarrollar tu talento mientras manejamos tu clientela.</p>
+						<a href="#" className="btn btn-success">
+							Ir
+						</a>
 					</div>
 				</div>
 			</div>
