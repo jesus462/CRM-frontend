becabd26f5e9595766d577b54f59be43caf9b333
Johneg02 <jonathanegarciasilveira@gmail.com>
2020-02-28 03:29:56 +0000

    home fixed

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,45 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+		<div className="container-home">
+			<div className="cell-home1">
+				<p>1</p>
+			</div>
+			<div className="cell-home2">
+				<p>2</p>
+			</div>
+		</div>
+
+		<div className="container-card-home">
+			<div className="row">
+				<div className="col-sm-6">
+					<div className="card">
+						<div className="card-body">
+							<h5 className="card-title">Special title treatment</h5>
+							<p className="card-text">
+								With supporting text below as a natural lead-in to additional content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6">
+					<div className="card">
+						<div className="card-body">
+							<h5 className="card-title">Special title treatment</h5>
+							<p className="card-text">
+								With supporting text below as a natural lead-in to additional content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
